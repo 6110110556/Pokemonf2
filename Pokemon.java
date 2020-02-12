f6c8740bba7c798ea27d6e748041a9fe510495e4
6110110556 <60842356+6110110556@users.noreply.github.com>
2020-02-12 22:59:36 +0700

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,39 +1,90 @@
+import java.util.Scanner;
+
+
 class Pokemon{
+    Scanner sc = new Scanner(System.in);
+
     //Attribuite
     private String name;
-    private final int power, level, stamina;
-    private int berry;
+        //w = Ways
+    private String w1;
+        // hp = Health Point, mp = Mana Point
+    private int power, level, stamina, hp, mp;
+    public int maxStamina =100;
+
 
 
     //DefaultConstructure
     public Pokemon(){
-        this.power =10;
+        this.power =(int)(((double)Math.random())*50+100);
         this.level =1;
         this.stamina =100;
+        this.hp = (int)(this.power*0.5+500);
+        this.mp = (int)(this.power*0.2+200);
     }
+
 
 
     // Method
-    public void setName(final String name) {
-        this.name = name;
+        //Behevier
+
+    public void setName() {
+        System.out.print("\n\n\t\t Enter Your name Pokemon Signature : ");
+        this.name = sc.nextLine();
+        System.out.print("\n\n\n\n\n");
 
     }
-    
+ /*   public void getName()(
+        System.out.println(this.name);
+    )*/
+/*
+    public int levelUp(int level){
+
+    }
+
+    public int pSleep(){
+
+        return this.stamina = maxStamina;
+    }
+*/
+
+        //All Display
+
     public void displayStatus(){
          System.out.print("\n\t   ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
-                                +"\n\t\t  Your Pokemon Name is "+this.name
-                                +"\n\t\t    Level : "+this.level
-                                +"\n\t\t    Power : "+this.power
-                                +"\n\t\t    Stamina : "+this.stamina
-                                +"\n\n\t  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+                            +"\n\t\t  Your Pokemon Name is "+this.name
+                            +"\n\t\t    Level : "+this.level
+                            +"\n\t\t    HP : "+this.hp
+                            +"\n\t\t    MP : "+this.mp
+                            +"\n\t\t    Power : "+this.power
+                            +"\n\t\t    Stamina : "+this.stamina
+                            +"\n\n\t  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
     }
 
- /*   public int eatBerry(int berry){
-        this.power = power+berry;
-        this.stamina = stamina+berry;
+    public void displayFaceGame(){
+        System.out.print("\n\t ************************************************************"
+                            +"\n\t\t ===== WELLCOM TO POKEMONNI GAME =====");
+    }
 
-    }*/
+    public String displayWayToFunction(){
+        System.out.print("\n\n\t ############################################################"
+                        +"\n\t\t PokeHouse:(1)"
+                        +"\n\t\t Go to Forest [CoE Garden]:(2)"
+                        +"\n\t\t Show Your Pokemon Status:(3)"
+                        +"\n\t\t Quit Game:(E)"
+                        +"\n\n\t\t your ans : ");
+        this.w1 = sc.nextLine();
+        System.out.print("\n\n ############################################################\n\n\n\n\n");
+        return this.w1;
 
+    }
+
+    // SystemAnalysis
+
+    public void quitGame(){
+        System.out.print("\n\t\t------------------------------------End Game------------------------------------"
+                            +"\n\n\t\t\t\t To Player : Get to Read a Book Pai!!! ");
+    }
 
     
 
--- PokemonGame.java
+++ PokemonGame.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 public class PokemonGame{
 
+    public int pokeBag =5;
+    //public Char w1; // w = ways
+
     public static void main(String [] args){
+        String w1; // w = ways
+       // Scanner sc = new Scanner(System.in);
+        PokemonGame user = new PokemonGame();
+        //สร้างอิฟเอล แล้วก็เช็ค boolean ว่าจะสร้างไม่สร้าง แล้วก็ ถ้าเต็มก็ไปต่อ
         Pokemon p1 = new Pokemon();
-        p1.setName("Coroamon");
-        p1.displayStatus();
+        p1.displayFaceGame();
+        p1.setName();
+        w1 = p1.displayWayToFunction();
+        user.chooseFunction(w1, p1);            
 
 
 
     }
+
+    public void chooseFunction(String w, Pokemon p){
+        if(w.equals("1")){
+           // p.pokeHouse();
+            System.out.println("**********Error (is not doing)**********");
+
+        }
+        else if(w.equals("2")){
+            //p.goToGarden();
+            System.out.println("**********Error (is not doing)**********");
+
+        }
+        else if(w.equals("3")){
+            p.displayStatus();
+            
+        }
+        else if(w.equals("E")  || w.equals("e")){
+            p.quitGame();
+        }
+        else{
+            System.out.println("**********Error (is not doing)**********");
+        }
+            
+    }
+
 }
